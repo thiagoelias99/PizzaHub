0c5ebac85b1adfe30d6639386147eabd3a51df2f
Thiago Elias <thiagoelias99@gmail.com>
2023-04-19 18:44:19 -0300

    configured authentication

--- backend/src/server/routes/index.ts
+++ backend/src/server/routes/index.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { UsersController, LoginController } from "../controllers";
 import { signUp } from "../controllers/login/SignUp";
 import { signIn } from "../controllers/login/SignIn";
+import { ensureAuthenticated } from "../middlewares/EnsureAuthenticated";
 
 const routes = Router();
 
@@ -10,14 +11,14 @@ routes.post("/signup", LoginController.signUpValidation, signUp);
 routes.post("/signin", LoginController.signInValidation, signIn);
 
 routes.route("/users")
-    //.all(ensureAuthenticated)
-    .post(UsersController.postValidation, UsersController.post)
+    .all(ensureAuthenticated)
+    // .post(UsersController.postValidation, UsersController.post)
     .get(UsersController.getAllValidation, UsersController.getAll);
 
 routes.route("/users/:uuid")
-    // .all(ensureAuthenticated)
-    .get(UsersController.getByUuidValidation, UsersController.getByUuid)
-    .put(UsersController.putValidation, UsersController.put)
-    .delete(UsersController.deleteValidation, UsersController.del);
+    .all(ensureAuthenticated)
+    .get(UsersController.getByUuidValidation, UsersController.getByUuid);
+// .put(UsersController.putValidation, UsersController.put)
+// .delete(UsersController.deleteValidation, UsersController.del);
 
 export { routes };
